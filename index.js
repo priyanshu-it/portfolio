5578b64c3e836c6987afee8151d3974098a2670c
Priyanshu <sepriyanshu21@gmail.com>
2025-08-04 16:25:35 +0530

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // Author: Priyanshu
 
 // JavaScript code to copy the text to clipboard
-document.addEventListener('copy', function(event) { 
-    event.clipboardData.setData('text/plain', 'priyanshu.html'); 
-    event.preventDefault(); 
+document.addEventListener('copy', function (event) {
+    event.clipboardData.setData('text/plain', 'priyanshu.html');
+    event.preventDefault();
+});
+
+document.querySelectorAll('.skills-list li').forEach(li => {
+    const percent = li.getAttribute('data-percent');
+    const bar = li.querySelector('.progress-bar div');
+    if (bar && percent) {
+        bar.style.width = percent + '%';
+    }
 });
