264e7a4d73293fa6a8206b74220b4d75d0976cf8
Priyanshu <sepriyanshu21@gmail.com>
2026-02-27 00:21:39 +0530

    Refactor index.js to simplify clipboard functionality
    Removed redundant code for skills list progress bars.

--- index.js
+++ index.js
@@ -5,11 +5,3 @@ document.addEventListener('copy', function (event) {
     event.clipboardData.setData('text/plain', 'priyanshu.html');
     event.preventDefault();
 });
-
-document.querySelectorAll('.skills-list li').forEach(li => {
-    const percent = li.getAttribute('data-percent');
-    const bar = li.querySelector('.progress-bar div');
-    if (bar && percent) {
-        bar.style.width = percent + '%';
-    }
-});
